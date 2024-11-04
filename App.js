a44d9a7108aa5383e1716f0e84394de8305ae7c0
monsaii <qsrcapena@tip.edu.ph>
2024-11-04 19:54:13 +0800

    added

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return (   
+    <ImageBackground 
+      source={require('./assets/monitoring.png')} 
+      style={styles.image}
+      resizeMode="cover" 
+    >
+      <View style={styles.container}>
+        <View style={styles.content}>
+
+        </View>
+
+        
+        <StatusBar style="auto" />
+      </View>
+
+      <View style={styles.border}>
+        
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  image: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    
+    flex: 1, 
+  },
+  border: {
+    borderWidth: 2,
+    borderColor: 'black',
+    paddingBottom: 60,
+    marginLeft: 50, 
+    marginRight: 50,
+    marginBottom: 50,
+    marginTop: 50,
+    borderRadius: 50,
+    backgroundColor: '#A0C5E5'
+  },
+  content: {
+    backgroundColor: 'red',
+    borderWidth: 2,
+    borderColor: 'black',
+    marginLeft: 20,
+    marginRight: 20,
+    backgroundColor: '#A0C5E5',
+    marginTop: 230,
+    borderRadius: 30,
+    padding: 130,
+  },
+
+
 });
+
