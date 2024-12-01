b6a1835eb2f85c11fdfeb4043e53760bea62af9c
Karlo Santos <qkdlsantos@tip.edu.ph>
2024-12-01 21:41:48 +0800

    FIX GRAPH AND per min

--- screens/RealTimeGraph.js
+++ screens/RealTimeGraph.js
@@ -29,7 +29,7 @@ const RealTimeGraph = ({ data, title, yAxisSuffix = " Mbps", threshold }) => {
             labels: labelsToShow.slice(0, data.values.length),
             datasets: [{ data: data.values }],
           }}
-          width={Math.max(labelsToShow.length * 40, screenWidth)} // Adjust width multiplier here
+          width={Math.max(labelsToShow.length * 30, screenWidth)} // Adjust width multiplier here
           height={250}
           yAxisSuffix={yAxisSuffix}
           chartConfig={{
